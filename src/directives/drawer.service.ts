3f0f776dd397f431a9061f3b9a4a124f97b36f0e
aquinum <aquinum@gmail.com>
2016-09-13 17:18:26 +0200

    Single handler clean up
    Clean after changes.

--- src/directives/drawer.service.ts
+++ src/directives/drawer.service.ts
@@ -15,55 +15,53 @@ import 'rxjs/add/operator/debounceTime';
 @Injectable()
 export class DrawerService {
 
-    private _drawerWidth: number = 300;
-    private _width$: BehaviorSubject<number> = new BehaviorSubject<number>(this._drawerWidth);
-    private _touched$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-
-    private _handlerRx$: Subject<Event> = new Subject<Event>();
+    private _width: number = 300;
+    private _width$: BehaviorSubject<number> = new BehaviorSubject<number>(this._width);
+    private _handler$: Subject<Event> = new Subject<Event>();
     private _position$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+    //If drawer should be forced to open or close, only used for missed isFinal events during panend
     private _force$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    //If element is currently touched
+    private _touched$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
     //Get reference point
     private _start$: Observable<number> =
-        this._handlerRx$
-            .filter((ev: any) => ev.type === 'start')
-            .map((ev: any) => ev.ev.center.x - ev.ev.target.getBoundingClientRect().left - this._drawerWidth);
+        this._handler$
+            .filter((ev: any) => ev.type === 'panstart')
+            .map((ev: any) => ev.center.x - ev.target.getBoundingClientRect().left - this._width);
 
     //Event triggered when touch finished and isFinal was not triggered, returns true if half of a drawer is still visible
     private _end$: Observable<boolean> =
-        this._handlerRx$
-            .filter((ev: any) => ev.type === 'end')
-            .do((ev) => this._touched$.next(false))
-            .do((ev: any) => ev.ev.preventDefault())
+        this._handler$
+            .filter((ev: any) => ev.type === 'panend')
+            .do((ev: any) => {
+                this._touched$.next(false);
+                ev.preventDefault();
+            })
             .mergeMap(() => new BehaviorSubject((fn: any) =>
                 this._force$
                     .last()
                     .filter(e => e)
                     .subscribe(fn)))
             .mergeMap(() => this._position$.take(1)
-                .map(pos => pos > this._drawerWidth/2));
+                .map(pos => pos > this._width/2));
 
     constructor(){
-//TODO: deactivate "second" event. example: if movement is done by handler deactivate drawer. Device can catch second event during motion and it will broke movement
-
-
         Observable.combineLatest(
             this._start$,
-            this._handlerRx$
-                .filter((ev: any) => ev.type === 'move'))
+            this._handler$
+                .filter((ev: any) => ev.type === 'panleft' || ev.type === 'panright'))
             .do((ev) => this._touched$.next(true))
             .map((val: any) => {
                 let [start, ev] = val;
                 return {
-                    pos: ev.ev.center.x - start,
-                    isFinal: ev.ev.isFinal,
-                    ev: ev.ev,
-                    type: ev.ev.type //TODO: add velocity
+                    pos: ev.center.x - start,
+                    isFinal: ev.isFinal,
+                    type: ev.type //TODO: add velocity
                 }
             })
             .subscribe(ev => {
-
-                this._position$.next(ev.pos > this._drawerWidth ? this._drawerWidth : ev.pos < 0 ? 0 : ev.pos);
+                this._position$.next(ev.pos > this._width ? this._width : ev.pos < 0 ? 0 : ev.pos);
                 if(ev.isFinal){
                     if(ev.type === 'panleft'){
                         this.close();
@@ -80,23 +78,30 @@ export class DrawerService {
         //Handle events that have ended but isFinal was not triggered
         this._end$
             .subscribe(bigger => {
-
                 if(bigger){
                     this.open();
                 } else {
                     this.close();
                 }
-
                 this._force$.next(false);
             });
     }
 
-    get position$(){
-        return this._position$;
+    getMove(ev){
+        this._handler$.next(ev);
     }
 
-    get handler$(){
-        return this._handlerRx$;
+    /* Cloaking stream for dealing with drawer animation during window resizing */
+    get resizeCloak$(){
+        return this._width$
+            .map(() => true)
+            .merge(this._width$
+                .debounceTime(350) //Animation time + .05s
+                .map(() => false));
+    }
+
+    get position$(){
+        return this._position$;
     }
 
     get width$(){
@@ -105,16 +110,16 @@ export class DrawerService {
 
     set width(val: number){
         this.close();
-        this._drawerWidth = val;
+        this._width = val;
         this._width$.next(val);
     }
 
-    get active$(){
+    get onTouch$(){
         return this._touched$;
     }
 
     open(){
-        this._position$.next(this._drawerWidth);
+        this._position$.next(this._width);
     }
 
     close(){
